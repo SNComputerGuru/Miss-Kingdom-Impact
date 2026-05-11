698bf6d288926e28e8a36577a50eb2cc3df9800e
SNComputerGuru <shillingndivho4@gmail.com>
2026-05-11 12:18:20 +0200

    Update hero.jsx to use hosted images instead of images from github

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import "../styles/hero.css";
-import heroImage from "../assets/_MG_2432.png";
+const heroImage = "https://ik.imagekit.io/yq17ihow7e/New%20Folder/_MG_2432.png";
 
 export default function Hero() {
     return (
